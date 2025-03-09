49fa4384e6b48d80c7d1e73c5b7ffd06cc80d864
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-09 16:03:37 +0200

    Add routes for team leader and member dashboard components

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ import Landing from './components/Landing/Landing';
 import Register from './components/auth/Register/Register';
 import Login from './components/auth/Login/Login';
 import ForgetPassword from './components/auth/ForgetPassword/ForgetPassword';
+import Dashboard from './components/teamLeader/dashboard/components/Dashboard';
+import CreateTask from './components/teamLeader/dashboard/components/createTask';
+import Tasks from './components/teamLeader/dashboard/components/Tasks';
+import Members from './components/teamLeader/dashboard/components/Members';
+import Board from './components/teamLeader/dashboard/components/Board';
+import Calendar from './components/teamLeader/dashboard/components/Calendar';
+import Profile from './components/teamLeader/dashboard/components/Profile';
+import MDashboard from './components/teamMember/dashboard/components/MDashboard';
+import MTasks from './components/teamMember/dashboard/components/MTasks';
+import MBoard from './components/teamMember/dashboard/components/MBoard';
+import MProfile from './components/teamMember/dashboard/components/MProfile';
+import MCalendar from './components/teamMember/dashboard/components/MCalendar';
+import Colleagues from './components/teamMember/dashboard/components/Colleagues';
 
 const App = () => {
     return (
@@ -15,6 +28,19 @@ const App = () => {
                 <Route path="/register" element={<Register />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<ForgetPassword />} />
+                <Route path="/leader-dashboard" element={<Dashboard />} />
+                <Route path="/create-new-task" element={<CreateTask />} />
+                <Route path="/tasks" element={<Tasks />} />
+                <Route path="/members" element={<Members />} />
+                <Route path="/leader-board" element={<Board />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/member-dashboard" element={<MDashboard />} />
+                <Route path="/member-tasks" element={<MTasks />} />
+                <Route path="/member-board" element={<MBoard />} />
+                <Route path="/member-profile" element={<MProfile />} />
+                <Route path="/member-calendar" element={<MCalendar />} />
+                <Route path="/colleagues" element={<Colleagues />} />
             </Routes>
             <ToastContainer />
         </BrowserRouter>
