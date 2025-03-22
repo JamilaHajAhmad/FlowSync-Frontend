c5937862d19689493672f6090b5494416d6a14a3
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-22 22:02:35 +0200

    Add Pie chart component and sample data for enhanced analytics visualization

---
+++ src/components/teamLeader/analytics/pie/data.js
@@ -0,0 +1,33 @@
+const data=[
+    {
+      "id": "lisp",
+      "label": "lisp",
+      "value": 323,
+      "color": "hsl(286, 70%, 50%)"
+    },
+    {
+      "id": "sass",
+      "label": "sass",
+      "value": 494,
+      "color": "hsl(23, 70%, 50%)"
+    },
+    {
+      "id": "elixir",
+      "label": "elixir",
+      "value": 79,
+      "color": "hsl(310, 70%, 50%)"
+    },
+    {
+      "id": "css",
+      "label": "css",
+      "value": 466,
+      "color": "hsl(231, 70%, 50%)"
+    },
+    {
+      "id": "c",
+      "label": "c",
+      "value": 389,
+      "color": "hsl(74, 70%, 50%)"
+    }
+  ]
+  export default data;
