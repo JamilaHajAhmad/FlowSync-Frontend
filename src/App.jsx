b4a3d59090a578ee0007747804b7a87a533e66d8
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-28 23:38:42 +0300

    Add NotificationProvider and NotificationList to App component for enhanced user notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { NotificationProvider } from "./components/common/notification/NotificationContext";
+import NotificationList from "./components/common/notification/NotificationList";
 import 'react-toastify/dist/ReactToastify.css';
 import Landing from './components/Landing/Landing';
 import Register from './components/auth/Register/Register';
@@ -17,11 +19,10 @@ import MTasks from './components/teamMember/dashboard/components/MTasks';
 import MCalendar from './components/teamMember/dashboard/components/MCalendar';
 import FreezeTask from './components/teamMember/dashboard/components/FreezeTask';
 import Settings from './components/common/Settings';
-import ARequests from './components/teamLeader/dashboard/components/ARequests';
+import Requests from './components/teamLeader/dashboard/components/Requests';
 import EditProfile from './components/common/EditProfile';
 import ChangePW from './components/common/ChangePW';
 import Analytics from './components/teamLeader/dashboard/components/Analytics';
-import FRequests from './components/teamLeader/dashboard/components/FRequests';
 import Layout from './components/teamLeader/analytics/layout/layout';
 import Bar from './components/teamLeader/analytics/bar/Bar';
 import HeatMap from './components/teamLeader/analytics/heatmap/HeatMap';
@@ -33,36 +34,38 @@ import Stacked from './components/teamLeader/analytics/stacked/Stacked';
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-                <Route path="/register" element={<Register />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgetPassword />} />
-                <Route path="/leader-dashboard" element={<Home />} />
-                <Route path="/tasks" element={<Tasks />} />
-                <Route path="/members" element={<Members />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/profile" element={<ProfilePage />} />
-                <Route path="/member-dashboard" element={<MHome />} />
-                <Route path="/member-tasks" element={<MTasks />} />
-                <Route path="/member-calendar" element={<MCalendar />} />
-                <Route path="/freeze-task" element={<FreezeTask />} />
-                <Route path="/settings" element={<Settings />} />
-                <Route path="/account-requests" element={<ARequests />} />
-                <Route path="/freeze-requests" element={<FRequests />} />
-                <Route path="/settings/edit-profile" element={<EditProfile />} />
-                <Route path="/settings/Change-password" element={<ChangePW />} />
-                <Route path="/analytics" element={<Analytics />} />
-                <Route path="/analytics/bar" element={<Layout children={<Bar />} />} />
-                <Route path="/analytics/line" element={<Layout children={<Line />} />} />
-                <Route path="/analytics/pie" element={<Layout children={<Pie />} />} />
-                <Route path="/analytics/stream" element={<Layout children={<Stream />} />} />
-                <Route path="/analytics/stacked" element={<Layout children={<Stacked />} />} />
-                <Route path="/analytics/heatmap" element={<Layout children={<HeatMap />} />} />
-            </Routes>
-            <ToastContainer />
-        </BrowserRouter>
+        <NotificationProvider>
+            <BrowserRouter>
+                <NotificationList />
+                <Routes>
+                    <Route path="/" element={<Landing />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/forgot-password" element={<ForgetPassword />} />
+                    <Route path="/leader-dashboard" element={<Home />} />
+                    <Route path="/tasks" element={<Tasks />} />
+                    <Route path="/members" element={<Members />} />
+                    <Route path="/calendar" element={<Calendar />} />
+                    <Route path="/profile" element={<ProfilePage />} />
+                    <Route path="/member-dashboard" element={<MHome />} />
+                    <Route path="/member-tasks" element={<MTasks />} />
+                    <Route path="/member-calendar" element={<MCalendar />} />
+                    <Route path="/freeze-task" element={<FreezeTask />} />
+                    <Route path="/settings" element={<Settings />} />
+                    <Route path="/requests" element={<Requests />} />
+                    <Route path="/settings/edit-profile" element={<EditProfile />} />
+                    <Route path="/settings/Change-password" element={<ChangePW />} />
+                    <Route path="/analytics" element={<Analytics />} />
+                    <Route path="/analytics/bar" element={<Layout children={<Bar />} />} />
+                    <Route path="/analytics/line" element={<Layout children={<Line />} />} />
+                    <Route path="/analytics/pie" element={<Layout children={<Pie />} />} />
+                    <Route path="/analytics/stream" element={<Layout children={<Stream />} />} />
+                    <Route path="/analytics/stacked" element={<Layout children={<Stacked />} />} />
+                    <Route path="/analytics/heatmap" element={<Layout children={<HeatMap />} />} />
+                </Routes>
+                <ToastContainer />
+            </BrowserRouter>
+        </NotificationProvider>
     );
 }
 
