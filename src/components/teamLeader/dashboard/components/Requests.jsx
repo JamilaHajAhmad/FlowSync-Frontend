94ab2d0c26adcea68e236c0b58db31cf2ef33914
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-03-28 23:53:43 +0300

    Add MiniDrawer component for managing requests in the dashboard

---
+++ src/components/teamLeader/dashboard/components/Requests.jsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { styled, ThemeProvider, createTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Topbar from '../../../common/Topbar';
+import Sidebar from './Sidebar';
+import getDesignTokens from '../../../../theme';
+import PageHeading from '../../../common/PageHeading';
+import Requests from '../pages/Requests';
+
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+
+export default function MiniDrawer() {
+    const [ open, setOpen ] = React.useState(false);
+
+    const handleDrawerOpen = () => {
+        setOpen(!open);
+    };
+
+    const [ mode, setMode ] = React.useState(
+        localStorage.getItem("currentMode") ? 
+        localStorage.getItem("currentMode") :
+        "light");
+    const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [ mode ]);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Topbar open={open} handleDrawerOpen={handleDrawerOpen} setMode={setMode} />
+                <Sidebar open={open} />
+                <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                    <DrawerHeader />
+                    <PageHeading 
+                        title="Pending Requests" 
+                        subtitle="Manage and review all employee requests"
+                    />
+                    <Requests />
+                </Box>
+            </Box>
+        </ThemeProvider>
+    );
+}
