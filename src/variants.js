35c17cb9f56f75197f5833aa53c45a4063bb7f4d
Jamila HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-04-04 23:08:30 +0300

    Added new variants

--- src/variants.js
+++ src/variants.js
@@ -167,4 +167,26 @@ export const authorMotion = {
     }
 };
 
+export const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            delayChildren: 0.3,
+            staggerChildren: 0.2
+        }
+    }
+};
+
+export const itemVariants = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.5
+        }
+    }
+};
+
 
