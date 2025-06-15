33f148f705f0f4f6838f97f0af99c3539dfe2125
Jamila_HajAhmad <jamila.haj.ahmad15@hotmail.com>
2025-06-15 23:46:13 +0300

    feat: Refactor Home component layout and enhance styling for improved responsiveness

--- src/components/Landing/components/Home/Home.jsx
+++ src/components/Landing/components/Home/Home.jsx
@@ -6,36 +6,42 @@ import { motion as Motion } from 'framer-motion';
 
 const Home = () => {
     return (
-        <Motion.section className='home'
+        <Motion.section 
+            className='home'
             id='home'
             variants={backgroundMotion}
             initial="hidden"
-            animate="visible">
+            animate="visible"
+        >
             <Header />
             <div className="hero">
-                <Motion.div
-                    className="content"
-                    variants={contentMotion}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    <h1>100<small>%</small></h1>
-                    <h2>
-                        Streamline
-                        <br />
-                        Sync
-                        <br />
-                        Achieve
-                    </h2>
-                </Motion.div>
-                <Motion.div
-                    className="image"
-                    variants={logoMotion}
-                    initial="hidden"
-                    animate="visible"
-                >
-                    <img src={Logo} alt="Logo" />
-                </Motion.div>
+                <div className="hero-grid">
+                    <Motion.div
+                        className="content-card"
+                        variants={contentMotion}
+                        initial="hidden"
+                        animate="visible"
+                    >
+                        <div className="percentage">
+                            <h1>100<small>%</small></h1>
+                        </div>
+                        <h2>
+                            Streamline
+                            <br />
+                            Sync
+                            <br />
+                            Achieve
+                        </h2>
+                    </Motion.div>
+                    <Motion.div
+                        className="logo-card"
+                        variants={logoMotion}
+                        initial="hidden"
+                        animate="visible"
+                    >
+                        <img src={Logo} alt="Logo" className='image'/>
+                    </Motion.div>
+                </div>
             </div>
         </Motion.section>
     )
